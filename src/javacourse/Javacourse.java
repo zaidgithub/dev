1d7cc3b0103c28b355281a565ef8beffe94ae83f
zaidodetallah <zaidodetallah@outlook.com>
2017-11-26 19:17:20 +0200

    this is the first branch

---
+++ src/javacourse/Javacourse.java
@@ -0,0 +1,39 @@
+
+package javacourse;
+
+
+public class Javacourse {
+
+   
+    public static void main(String[] args) {
+    /* car camry = new car();
+     camry.setspeed(80);
+        */
+    /* int sp = camry.getspeed();
+     System.out.println(sp); 
+    */
+  
+      for (int i=1; i<=10; i++)
+   {
+   for (int y=10; y>=i; y--)
+   {
+       System.out.print('*');
+   }
+       System.out.println(' ');
+    }
+ 
+      
+ System.out.println("5alasna"); 
+      
+      
+   for (int i=10; i>=1; i--)
+   {
+   for (int y=1; y<=10; y++)
+   {
+       System.out.print('*');
+   }
+       System.out.println(' ');
+    }
+    
+}
+}
---
+++ src/javacourse/car.java
@@ -0,0 +1,41 @@
+
+package javacourse;
+
+public class car {
+
+    public int speed;
+    
+    public car()
+    {
+    this.speed=0;
+    }
+    
+    public void setspeed(int p)
+    {
+    this.speed=p;
+    switch(this.speed)
+    {
+    case 50:
+    case 60:
+    case 70:
+    case 80:
+    case 90:
+          System.out.println("you are in saafe mode");
+    break;
+    case 100:
+         System.out.println("100km");
+    break;
+    case 180:
+        System.out.println("please slow down now");
+    default:
+        System.out.println("no value is selected ");
+    }
+    
+    }
+    
+    public int getspeed()
+    {
+    return this.speed;
+    }
+    
+}
